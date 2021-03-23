8c78e1c53df7348f264339457acd036822dd29ee
partum <jarringt@uccs.edu>
2021-03-23 13:09:44 -0600

    changed accordion logic

--- script.js
+++ script.js
@@ -3,23 +3,27 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
+    console.log(this);
+    if (this.nextElementSibling.style.display === "block") {
+      this.classList.remove("active");
+      this.nextElementSibling.style.display = "";
+      //JS reads display:"none" as display:""
+    } else if (this.nextElementSibling.style.display === "") {
+      for (i = 0; i < acc.length; i++) {
+        if (acc[i].nextElementSibling.style.display === "block") {
+          acc[i].nextElementSibling.style.display = "";
+          acc[i].classList.remove("active");
+        }
+      }
+      this.classList.toggle("active");
       panel.style.display = "block";
     }
   });
 }
 
 /*
-Add arrow icon
 add code to have one section close when another opens
-fix @ box picture
-file:///C:/Users/kawai/Documents/GitHub/accordian-card-hub/index.html
+fix image transparency
+fix overflow on body
 */
